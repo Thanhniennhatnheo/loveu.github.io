0697129f9e908074cca9777be4fa86a4654e62dc
Thanhniennhatnheo <124350879+Thanhniennhatnheo@users.noreply.github.com>
2023-02-03 20:38:12 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -8,10 +8,10 @@ const textConfig = {
   text7: "Lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì tớ thích cậu mất rồi",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Tớ biết mà ^^ Yêu nhau nhé !",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì cậu xem hết bí mật này nha!",
-  text12: "Okii lunn <3",
+    "Nãy giờ đùa thôi đừng cay nhé :))",
+  text12: "...",
 };
 
 $(document).ready(function () {
